4fdac71be1722cc20be9c141a0b2ccbf5a32d528
Alec Geatches <throwpi@me.com>
2016-01-17 14:29:15 +0800

    Initial commit

---
+++ tlwdr3500-name-asserted-functions.py
@@ -0,0 +1,40 @@
+ea = ScreenEA()
+
+for function_start in Functions(SegStart(ea), SegEnd(ea)):
+	function_name = GetFunctionName(function_start)
+	if not function_name.startswith('sub_'):
+		continue
+
+	functions_with_assert = set()
+	function_renames = {}
+
+	function_end = FindFuncEnd(function_start)
+	current_address = function_start
+	while current_address != BADADDR:
+		code_refs = list(CodeRefsFrom(current_address, 1))
+		for ref in code_refs:
+			if GetFunctionName(ref) == "__assert":
+				functions_with_assert |= {function_name}
+				
+				# Step back from current_address, find what was loaded into a2
+				instruction_address_for_argument = current_address
+				for x in range(10):
+					instruction_address_for_argument = PrevHead(instruction_address_for_argument)
+					mnem = GetMnem(instruction_address_for_argument)
+					if mnem == "la":
+						register = GetOpnd(instruction_address_for_argument, 0)
+						if register == "$a3":
+							string_name = GetOpnd(instruction_address_for_argument, 1)
+							string_location = GetOperandValue(instruction_address_for_argument, 1)
+							string_value = GetString(string_location, -1, ASCSTR_C) 
+							function_renames[function_name] = string_value
+							
+		current_address = NextHead(current_address, function_end)
+
+	functions_with_renames = [f for f in functions_with_assert if f in function_renames]
+	unidentified_funtions = [f for f in functions_with_assert if f not in function_renames]
+
+	for f in functions_with_renames:
+		print "Found %s using __assert with function name %s" % (function_name, function_renames[function_name])
+	for f in unidentified_funtions:
+		print "Found unidentified function %s using assert" % functions_with_renames
